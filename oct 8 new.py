c3ce42d01a1476dbd6fb3bf7fa2646af5a0aca73
annn88 <annmariyabiju2028@cs.sjcetpalai.ac.in>
2024-10-08 15:18:01 +0530

    Add files via upload

---
+++ oct 8 new.py
@@ -0,0 +1,8 @@
+first_name=input("enter your first_name:")
+last_name=input("enter your last_name:")
+full_name=first_name+" "+last_name
+print(full_name)
+length=len(first_name)
+print(length)
+extracted_last_name=full_name[:length]
+print(extracted_last_name)
